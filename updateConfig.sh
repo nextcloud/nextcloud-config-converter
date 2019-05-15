155ac7ee2f1e0f32946ae8873e6db97a0bd97292
Morris Jobke <hey@morrisjobke.de>
2019-05-15 14:24:26 +0200

    Add stable16
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- updateConfig.sh
+++ updateConfig.sh
@@ -19,7 +19,7 @@ fi
 git clone git@github.com:nextcloud/documentation.git /tmp/nextcloud-documentation
 cd /tmp/nextcloud-documentation
 
-for branch in stable14 stable15 master
+for branch in stable14 stable15 stable16 master
 do
 	git checkout  $branch
 	cd $currentDir
