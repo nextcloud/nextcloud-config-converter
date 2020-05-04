c46b30b1d7d350b2ce10febe335a0e2c00f82de2
Morris Jobke <hey@morrisjobke.de>
2020-05-04 13:13:00 +0200

    Add newest branches
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- updateConfig.sh
+++ updateConfig.sh
@@ -19,7 +19,7 @@ fi
 git clone git@github.com:nextcloud/documentation.git /tmp/nextcloud-documentation
 cd /tmp/nextcloud-documentation
 
-for branch in stable14 stable15 stable16 master
+for branch in stable17 stable18 master
 do
 	git checkout  $branch
 	cd $currentDir
